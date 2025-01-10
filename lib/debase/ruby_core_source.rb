50e49229ad5082c129da76f81b43e1b4dd2ed44e
Matthew Thornton <99351305+ThorntonMatthew@users.noreply.github.com>
2025-01-10 10:32:21 -0500

    Switch to using p over puts

--- lib/debase/ruby_core_source.rb
+++ lib/debase/ruby_core_source.rb
@@ -70,7 +70,7 @@ module Debase
     def self.ruby_source_dir_version(dir)
       match = /ruby-([0-9\.]+)-((p|rc|preview)[0-9]+)\z/.match(dir)
 
-      puts "dir: #{dir} -  match: #{match}"
+      p "dir: #{dir} -  match: #{match}"
 
       Gem::Version.new("#{match[1]}.#{match[2]}")
     end
